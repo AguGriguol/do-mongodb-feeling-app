4d5baa25e5e590d72a266a37d881ffd0b408ddd5
Ortigoza, Matías Nicolás <ortigozamatias@gmail.com>
2021-07-09 16:18:12 -0300

    Wrap app with material-ui theme

--- web/src/app/components/Root/Root.component.tsx
+++ web/src/app/components/Root/Root.component.tsx
@@ -1,5 +1,15 @@
+import { BrowserRouter } from 'react-router-dom';
+import { CssBaseline } from '@material-ui/core';
+import { ThemeProvider } from '@material-ui/core/styles';
+import { theme } from 'utils/theme/base';
+
 const Root = () => (
-  <div>hola paola</div>
+  <ThemeProvider theme={theme}>
+    <CssBaseline />
+    <BrowserRouter>
+      <h1>hhere i am </h1>
+    </BrowserRouter>
+  </ThemeProvider>
 );
 
 export default Root;
